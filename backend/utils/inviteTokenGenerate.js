1f3d74c0403e2dad45d567ce4241ceca61cc8247
Abdullahsaif77 <158745281+Abdullahsaif77@users.noreply.github.com>
2025-10-08 11:17:33 -0700

    Clean commit after removing ghost nul file

--- backend/utils/inviteTokenGenerate.js
+++
@@ -1,9 +0,0 @@
-const crypto = require('crypto');
-
-const inviteTokenGenarate = (hours)=>{
-    const token = crypto.randomBytes(20).toString('hex')
-    const expiresAt = new Date(Date.now()+hours*60*60*100)
-    return {token , expiresAt};
-}
-
-module.exports = inviteTokenGenarate;
